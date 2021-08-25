ddb1d0aae854d0bc8c078e94d540cd3b17122453
Kiminjo <deulseong1121@gmail.com>
2021-08-25 16:54:12 +0900

    injo-Modify code to modify pre-processing only in main

--- doc2vec.py
+++ doc2vec.py
@@ -6,7 +6,7 @@ class Doc_to_vec :
     def __init__(self, document, epochs=100, embedding_size=64, alpha=0.01, min_alpha=0.00025, min_count=1, dm=1, model_name=None) :
         # data
         self.document = document
-        self.tagged_data = self.text_tagging()
+        #self.tagged_data = self.text_preprocessing()
 
         # parameters
         self.epochs= int(epochs)
@@ -17,20 +17,20 @@ class Doc_to_vec :
         self.dm = int(dm)
         self.model_name = model_name
 
-
-
-    def text_tagging(self) :
+    def text_preprocessing(self) :
+        '''
+        Do not use this method 
+        '''
         return [TaggedDocument(words=nltk.word_tokenize(word.lower()), tags=[str(index)]) for index, word in enumerate(self.document)]
-
 
     def train(self) :
         self.doc2vec_model = Doc2Vec(vector_size=self.embedding_size, alpha=self.alpha, min_alpha=self.min_alpha, min_count=self.min_count, dm=self.dm)
-        self.doc2vec_model.build_vocab(self.tagged_data)
+        self.doc2vec_model.build_vocab(self.document)
 
         for epoch in range(self.epochs) :
             if epoch % 10 == 0 :
                 print('Epochs : {}'.format(epoch))
-            self.doc2vec_model.train(self.tagged_data, 
+            self.doc2vec_model.train(self.document, 
                                 total_examples=self.doc2vec_model.corpus_count,
                                 epochs=self.doc2vec_model.epochs)
             self.doc2vec_model.alpha -= 0.0002
